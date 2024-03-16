9fe2da08d97ba69e563158228ddc4abbfe7f470a
Samuel Schwager <samuel.schwager@gmail.com>
2024-03-16 18:12:55 +0100

    add door interactable

---
+++ interactables/door/door.gd
@@ -0,0 +1,23 @@
+extends Node2D
+
+@export var door_open: Sprite2D
+@export var door_closed:Sprite2D
+@export var collider: CollisionShape2D
+@export var is_open = false
+
+
+func activate(state):
+	if not multiplayer.is_server():
+		return
+	is_open = state
+	set_door_props()
+
+
+func set_door_props():
+	door_open.visible = is_open
+	door_closed.visible = !is_open
+	collider.set_deferred("disabled", is_open)
+
+
+func _on_multiplayer_synchronizer_delta_synchronized() -> void:
+	set_door_props()
