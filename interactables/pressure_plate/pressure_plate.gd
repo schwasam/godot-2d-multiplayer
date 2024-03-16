5df936a2e4a6423b7f3be8a62846a1e1857ae697
Samuel Schwager <samuel.schwager@gmail.com>
2024-03-16 18:25:47 +0100

    wire up pressure plate and door

--- interactables/pressure_plate/pressure_plate.gd
+++ interactables/pressure_plate/pressure_plate.gd
@@ -1,5 +1,7 @@
 extends Node2D
 
+signal toggle(state)
+
 @export var is_down = false
 @export var plate_up: Sprite2D
 @export var plate_down: Sprite2D
@@ -15,6 +17,8 @@ func _on_area_2d_body_entered(_body: Node2D) -> void:
 
 
 func _on_area_2d_body_exited(_body: Node2D) -> void:
+	if multiplayer.multiplayer_peer == null:
+		return
 	if not multiplayer.is_server():
 		return
 	bodies_on_plate -= 1
@@ -23,6 +27,7 @@ func _on_area_2d_body_exited(_body: Node2D) -> void:
 
 func update_plate_state():
 	is_down = bodies_on_plate > 0
+	toggle.emit(is_down)
 	set_plate_props()
 	
 	
